86cc4f2625f529093f0a563b94c7459794b10d76
FawzyMokhtar <dev.fawzymokhtar@gmail.com>
2020-03-17 06:13:17 +0200

    Setup eslint, prettier and husky with lint-staged.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello');
+app.get('', (req: Request, res: Response) => {
+  res.send('Hello');
 });
 
-app.listen(PORT, () => console.log(`Express server is running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Express server is running on port ${PORT}`)
+);
